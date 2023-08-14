06b151038fd59ae84ffdfb961e83fbf0148776ed
Nishu026 <pmnishu.2000@gmail.com>
2023-08-14 14:48:26 +0530

    updated

--- form.py
+++ form.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from flask import Flask,render_template,request
+from flask_wtf import FlaskForm 
+from wtforms import StringField,SubmitField,validators
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nishu'
@@ -10,10 +10,13 @@ class MyForm(FlaskForm):
     email = StringField('Email: ')
     submit = SubmitField('Sign Up')
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
+@app.route("/", methods=['GET','POST'])
+def nush():
         form = MyForm()
-        return render_template('index.html', form=form)
+        if request.method == 'POST':
+            return "Form submitted!"
+        else:
+           return render_template('index.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
